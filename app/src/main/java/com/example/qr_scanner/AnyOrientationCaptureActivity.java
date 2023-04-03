481ae3123179417faf5926e0f143e0d086cdec61
filda <filda.navratil@gmail.com>
2023-04-03 14:40:29 +0200

    init commit 2

---
+++ app/src/main/java/com/example/qr_scanner/AnyOrientationCaptureActivity.java
@@ -0,0 +1,11 @@
+package com.example.qr_scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class AnyOrientationCaptureActivity extends CaptureActivity {
+
+}
---
+++ app/src/main/java/com/example/qr_scanner/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.qr_scanner;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.qr_scanner.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity {
+
+    private TextView textView;
+    private Button scanButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        scanButton = findViewById(R.id.scanButton);
+
+        scanButton.setOnClickListener(view -> {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+            integrator.setPrompt("Scan something");
+            integrator.setCaptureActivity(AnyOrientationCaptureActivity.class);
+            integrator.setOrientationLocked(false);
+            integrator.setCameraId(0);
+            integrator.setBeepEnabled(true);
+            integrator.setBarcodeImageEnabled(true);
+            integrator.initiateScan();
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                textView.setText(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
